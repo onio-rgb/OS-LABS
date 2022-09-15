c81acffae137fa4d0a6a24be7486b64d17d22239
onio-rgb <astitvastu@gmail.com>
2022-09-15 09:35:31 +0530

    .

--- command line processing/cmdlp.c
+++ command line processing/cmdlp.c
@@ -5,6 +5,7 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<errno.h>
 int main()
 {
 	printf("$");
@@ -14,42 +15,56 @@ int main()
 	char args[20][50];
 	int argc=0;
 	int n_read=0;
+	int cpy_stdout;
+	int cpy_stdin;
 	while(sscanf(cmd,"%s%n",word,&n_read)!=EOF)
 	{
 		strcpy(args[argc],word);
 		argc=argc+1;
 		cmd=cmd+n_read+1;
 	}
-	
 	int i_buff=0;
-	char* buff[20];
+	char buff[20][50];
+	int back_f=0;
 	for(int i=0;i<argc;i++)
 	{
 		
 		if(strcmp(args[i],">")==0)
 		{
-			int cpy_stdout=dup(1);
+			cpy_stdout=dup(1);
 			int outfd=open(args[i+1],O_WRONLY);
 			dup2(outfd,1);
-			printf("hello");
-			fflush(stdout);
-			dup2(cpy_stdout,1);
-			close(cpy_stdout);
+			// printf("hello");
+			// fflush(stdout);
+			// dup2(cpy_stdout,1);
+			// close(cpy_stdout);
 		}
 		else if(strcmp(args[i],"<")==0)
 		{
-			int cpy_stdin=dup(0);
+			cpy_stdin=dup(0);
 			int infd=open(args[i+1],O_RDONLY);
 			dup2(infd,0);
-			char * temp=malloc(10);
-			scanf("%s",temp);
-			fflush(stdin);
-			dup2(cpy_stdin,0);
-			close(cpy_stdin);
-			printf("%s",temp);
+			// char * temp=malloc(10);
+			// scanf("%s",temp);
+			// fflush(stdin);
+			// dup2(cpy_stdin,0);
+			// close(cpy_stdin);
+			// printf("%s",temp);
 		}
+		else if(args[i]=="&")
+		back_f=1;
 		else {
-
+			strcpy(buff[i_buff],args[i]);
+		}
+	}
+	strcpy(args[argc-1],(char*)NULL);
+	int pid=fork();
+	if(pid==0)
+	{
+		if(execvp(args[0],args)<0)
+		{
+			fflush(stdout);
+			write(cpy_stdout,&errno,sizeof(errno));
 		}
 	}
 }
